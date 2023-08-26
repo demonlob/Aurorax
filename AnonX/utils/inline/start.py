5997220546d975c756f7aa16140371d5d06a6b5f
demonlob <142598456+demonlob@users.noreply.github.com>
2023-08-27 01:09:55 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -6,22 +6,6 @@ import config
 from AnonX import app
 
 
-def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text="Aԃԃ ɱҽ ƚσ ɠɾσυρ",
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="Sҽƚƚιɳɠʂ", callback_data="settings_helper"
-            ),
-        ],
-     ]
-    return buttons
-
 
 def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
